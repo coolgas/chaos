5f76eecdb7840b61f4af485b57e1ca3b2a8932e5
Tianyi Yan <tiya3781@gmail.com>
2022-03-26 22:57:32 +0100

    entanglement entropy added

--- src/Chaos.jl
+++ src/Chaos.jl
@@ -1,6 +1,8 @@
-module Integer
+module Chaos
 
-export integer_partition, integer_partition_redundant, hopping_pair, average_spacing
+using LinearAlgebra
+
+export integer_partition, integer_partition_redundant, hopping_pair, average_spacing, state_evolution, reduced_density_matrix
 
 """
 This function will give partitions of m into at most n integers. Please notice
@@ -91,5 +93,72 @@ function average_spacing(differences::Vector{Float64}, i::Int64, v::Int64)
     end
     return sum/v
 end
+
+"""
+This function will give us the evolution state after time t.
+Here init denotes the initial state, basis denotes the basis of the Hilbert space.
+"""
+function state_evolution(;H::Matrix{Float64},t::Float64,basis::Vector{Vector{Int64}},init::Vector{Int64})
+    @assert t != 0.0
+    ind = findall(x->x==init, basis)[1]
+    eigenvals = eigvals(H)
+    eigenvecs = eigvecs(H)
+    len = length(eigenvals)
+    ψ_t = zeros(Float64, length(basis),1)
+    for i in 1:len
+        vec = eigenvecs[:,i]
+        val = eigenvals[i]
+        ψ_t += vec[ind]*exp(-im*val*t)*vec
+    end
+    return vec(ψ_t)
+end
+
+"""
+This function calculates the reduced_density_matrix by tracing out
+subsystem B. Here Na is the number of atoms. Notice that this function
+only works in 3-site system.
+"""
+function reduced_density_matrix(;state::Vector{T}, Na::Int64) where T <: Number
+    N = length(state)
+    
+    rd_rank = 0
+    int_rank = 0
+
+    rank1 = Na+1
+    rank2 = N/(Na+1)
+
+    if rank1 > rank2
+        int_rank = rank2
+        rd_rank = rank1
+    else
+        int_rank = rank1
+        rd_rank = rank2
+    end
+
+    rd_rank = Int(rd_rank)
+    int_rank = Int(int_rank)
+
+    d_mat = state * state' # constructing the density matrix.
+    
+    if typeof(state[1]) == Float64
+        traces = Array{Float64}(undef, 1, rd_rank*rd_rank)
+    else
+        traces = Array{Complex}(undef, 1, rd_rank*rd_rank)
+    end 
+    #traces = Array{Float64}(undef, 1, rd_rank*rd_rank)
+    #traces = Array{Number}(undef, 1, rd_rank*rd_ran)
+
+    i = 1
+    for j in 1:int_rank:N
+        for k in 1:int_rank:N
+            traces[i] = tr(d_mat[j:j+int_rank-1, k:k+int_rank-1])
+            i += 1
+        end
+    end
+
+    rd_mat = reshape(traces,(rd_rank, rd_rank))
+    return transpose(rd_mat)
+end
+
 
 end
