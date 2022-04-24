2b935c2c32a46d4ec3fa39fe09b97ee48f47df9a
Tianyi Yan <tiya3781@gmail.com>
2022-04-24 23:47:12 +0200

    main file added

--- Main.jl
+++ Main.jl
@@ -115,11 +115,125 @@ for i in 1:length(diffs1_dropped)
     end
 end
 
-fig = Figure();ax = Axis(fig[1,1], xlabel="s", ylabel="P")
+fig = Figure()
+ax = Axis(fig[1,1], xlabel="s", ylabel="P")
 x = range(0, 7, length=50)
 hist!(ax, new_diffs1, bins=:15, normalization=:pdf, xlabel="s")
 lines!(ax, x, ff, color=:black, linestyle=:dash, linewidth=3.5)
 lines!(ax, x, f, color=:red, linestyle=:dash, linewidth=3.5)
 fig
 
+# Everything set up, we are going to entanglement entropy
+# This basically implements the general formula of entanglement entropy.
+function EE(; γ::Float64, J::Float64, g::Float64, d::Float64, C6::Float64, R::Float64, Na::Int64)
+    vec = eigvecs(Htot(γ=γ, J=J, g=g, d=d, C6=C6, R=R))[:,1]
+    rdmat = reduced_density_matrix(state=vec, Na=Na, Ls=Ls, Rd=1)
+    vals = svd(rdmat).S
+    EE = -transpose(vals)*log.(vals)
+    return EE
+end
+
+# This function find the evoling state
+function evolEE(;H::Matrix{Float64},t::Float64,
+basis::Vector{Vector{Int64}},init::Vector{Int})
+    if t == 0.0
+        EE = 0.0
+    else
+        vec = state_evolution(H=H,t=t,basis=basis,init=init)
+        #rdmat = reduced_density_matrix(Na=Na, state=vec, Hbs=Hbs)
+        rdmat = reduced_density_matrix(state=vec, Na=Na, Ls=Ls, Rd=1)
+        vals = svd(rdmat).S
+        #EE = -transpose(conj(vals).*vals)*log.(conj(vals).*(vals))
+        #vals_squd = vals.*conj(vals)
+        EE = -transpose(vals)*log.(vals)
+    end
+    return EE
+end
+
+# Calculating a bunch of Hamiltonians.
+H0 = Htot(γ=0.0, J=J, g=g, d=d, C6=C6, R=R);
+H1 = Htot(γ=1.0, J=J, g=g, d=d, C6=C6, R=R);
+H2 = Htot(γ=2.0, J=J, g=g, d=d, C6=C6, R=R);
+H3 = Htot(γ=3.0, J=J, g=g, d=d, C6=C6, R=R);
+H4 = Htot(γ=4.0, J=J, g=g, d=d, C6=C6, R=R);
+H5 = Htot(γ=5.0, J=J, g=g, d=d, C6=C6, R=R);
+H6 = Htot(γ=6.0, J=J, g=g, d=d, C6=C6, R=R);
+H7 = Htot(γ=7.0, J=J, g=g, d=d, C6=C6, R=R);
+H8 = Htot(γ=8.0, J=J, g=g, d=d, C6=C6, R=R);
+H9 = Htot(γ=9.0, J=J, g=g, d=d, C6=C6, R=R);
+H10 = Htot(γ=10.0, J=J, g=g, d=d, C6=C6, R=R);
+
+# Initializing a bunch of things.
+ts = 0.0:0.2:10;
+gammas_new = 0.0:1:8
+ts1 = 0.0:0.2:5
+
+# Initializing a bunch of things
+aveEEs = zeros(Float64, 1, length(gammas_new))
+std_ee = zeros(Float64, 1, length(gammas_new))
+aveEEs_new = zeros(Float64, 1, length(gammas_new))
+std_ee_new = zeros(Float64, 1, length(gammas_new))
+aveEEs_newnew = zeros(Float64, 1, length(gammas_new))
+std_ee_newnew = zeros(Float64, 1, length(gammas_new))
+
+# Below we consider the case [20, 20, 20]
+@time evolEE0s = [evolEE(H=H0,t=t,basis=Hbs,init=vec([20 20 20])) for t in ts];
+@time evolEE4s = [evolEE(H=H4,t=t,basis=Hbs,init=vec([20 20 20])) for t in ts];
+@time evolEE6s = [evolEE(H=H6,t=t,basis=Hbs,init=vec([20 20 20])) for t in ts];
+@time evolEE8s = [evolEE(H=H8,t=t,basis=Hbs,init=vec([20 20 20])) for t in ts];
+@time evolEE10s = [evolEE(H=H10,t=t,basis=Hbs,init=vec([20 20 20])) for t in ts];
+
+# Below we consider the case [0, 0, 60]
+@time evolEE0s_new = [evolEE(H=H0,t=t,basis=Hbs,init=vec([0 0 60])) for t in ts];
+@time evolEE4s_new = [evolEE(H=H4,t=t,basis=Hbs,init=vec([0 0 60])) for t in ts];
+@time evolEE6s_new = [evolEE(H=H6,t=t,basis=Hbs,init=vec([0 0 60])) for t in ts];
+@time evolEE8s_new = [evolEE(H=H8,t=t,basis=Hbs,init=vec([0 0 60])) for t in ts];
+@time evolEE10s_new = [evolEE(H=H10,t=t,basis=Hbs,init=vec([0 0 60])) for t in ts];
+
+# Below we conside the case [10,40,10]
+@time evolEE0s_newnew = [evolEE(H=H0,t=t,basis=Hbs,init=vec([10 40 10])) for t in ts];
+@time evolEE4s_newnew = [evolEE(H=H4,t=t,basis=Hbs,init=vec([10 40 10])) for t in ts];
+@time evolEE6s_newnew = [evolEE(H=H6,t=t,basis=Hbs,init=vec([10 40 10])) for t in ts];
+@time evolEE8s_newnew = [evolEE(H=H8,t=t,basis=Hbs,init=vec([10 40 10])) for t in ts];
+@time evolEE10s_newnew = [evolEE(H=H10,t=t,basis=Hbs,init=vec([10 40 10])) for t in ts];
+
+
+# [20 20 20] case of averages
+@time for i in 1:length(gammas_new)
+	gamma = gammas_new[i]
+	H = Htot(γ=gamma, J=J, g=g, d=d, C6=C6, R=R)
+	evolEEs = [evolEE(H=H,t=t,basis=Hbs,init=vec([20 20 20])) for t in ts1]
+	len = length(evolEEs)
+	drop_rate = 0.2 # we drop the first 20% of data
+	left_num = Int(floor(drop_rate*len))
+	aveEEs[i] = mean(evolEEs[left_num:len])
+	std_ee[i] = std(evolEEs[left_num:len])
+	println(aveEEs)
+end
+
+# [0 0 60] case of averages
+@time for i in 1:length(gammas_new)
+	gamma = gammas_new[i]
+	H = Htot(γ=gamma, J=J, g=g, d=d, C6=C6, R=R)
+	evolEEs = [evolEE(H=H,t=t,basis=Hbs,init=vec([0 0 60])) for t in ts1]
+	len = length(evolEEs)
+	drop_rate = 0.2 # we drop the first 20% of data
+	left_num = Int(floor(drop_rate*len))
+	aveEEs_new[i] = mean(evolEEs[left_num:len])
+	std_ee_new[i] = std(evolEEs[left_num:len])
+	println(aveEEs_new)
+end
+
+# [10 40 10] case of averages
+@time for i in 1:length(gammas_new)
+	gamma = gammas_new[i]
+	H = Htot(γ=gamma, J=J, g=g, d=d, C6=C6, R=R)
+	evolEEs = [evolEE(H=H,t=t,basis=Hbs,init=vec([10 40 10])) for t in ts1]
+	len = length(evolEEs)
+	drop_rate = 0.2 # we drop the first 20% of data
+	left_num = Int(floor(drop_rate*len))
+	aveEEs_newnew[i] = mean(evolEEs[left_num:len])
+	std_ee_newnew[i] = std(evolEEs[left_num:len])
+	println(aveEEs_newnew)
+end
 
